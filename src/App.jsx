951bce38f8d84566c4a866e220886154fa76793c
Subrat kumar jena <subratj611@gmail.com>
2024-02-17 03:06:03 +0530

    feat: layout develop

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
 function App() {
-  return <div className="text-green-500">Machine Coding</div>;
+  return (
+    <div className="w-full min-h-screen h-full">
+      <div className="relative w-[90%] max-w-[1280px] min-h-screen h-full border mx-auto shadow-sm">
+        <div className="pt-10 px-10">
+          <h3 className="text-lg font-medium tracking-wide">
+            Fullstack Developer
+          </h3>
+          {/* stepper */}
+          {/* body */}
+        </div>
+        <div className="fixed w-[90%] max-w-[1280px] bg-white bottom-0 border border-t-gray-200 h-[80px] flex items-center justify-end z-10">
+          <button className="bg-blue-600 px-8 py-2 font-medium text-lg text-gray-200 rounded-md mr-6">
+            Next
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
