3364b2f5567b0dc85c05f62794ad20fca1eb0d3a
Subrat kumar jena <subratj611@gmail.com>
2024-02-17 03:15:18 +0530

    feat: btn click effect add

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,15 @@
+import { useState } from "react";
+
 function App() {
+  const [isNextBtnClicked, setIsNextBtnClicked] = useState(false);
+  const handleNext = () => {
+    setIsNextBtnClicked((prev) => !prev);
+
+    setTimeout(() => {
+      setIsNextBtnClicked((prev) => !prev);
+    }, 100);
+  };
+
   return (
     <div className="w-full min-h-screen h-full">
       <div className="relative w-full max-w-[1280px] min-h-screen h-full border mx-auto shadow-sm">
@@ -10,7 +21,12 @@ function App() {
           {/* body */}
         </div>
         <div className="fixed w-full max-w-[1280px] bg-white bottom-0 border border-t-gray-200 h-[80px] flex items-center justify-end z-10">
-          <button className="bg-blue-600 px-8 py-2 font-medium text-lg text-gray-200 rounded-md mr-6">
+          <button
+            className={`bg-blue-600 px-8 py-2 font-medium border-b-4 border-gray-700 border-l-4 text-lg text-gray-200 rounded-md mr-6 hover:bg-blue-700 ${
+              isNextBtnClicked ? "focus:scale-95" : "scale-100"
+            }`}
+            onClick={handleNext}
+          >
             Next
           </button>
         </div>
